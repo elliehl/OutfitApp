e795ecf39f063e6a46abaffec8bfc15b6f0bac50
Ellie <101657425+elliehl@users.noreply.github.com>
2023-10-10 20:32:21 +0100

    Update entity

--- OutfitTracker/Entities/AddItemEntity.cs
+++ OutfitTracker/Entities/AddItemEntity.cs
@@ -3,7 +3,6 @@ namespace OutfitTracker.Entities
 {
 	public class AddItemEntity
 	{
-        public int Id { get; set; }
         public string? Name { get; set; }
         public int Item_Type_Id { get; set; }
         public double? Price { get; set; }
